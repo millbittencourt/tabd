cb9c3f2259f2a605af9e6b6737dd8221ff7450b1
Jamille Santos <jamille.santos@solutis.com.br>
2019-12-03 18:06:04 -0300

    Alteração dos scripts para adicionar as relações no MtSql

--- Script BD/Script do banco de dados.sql
+++ Script BD/Script do banco de dados.sql
@@ -1,5 +1,5 @@
---Script do banco de dados
---##Drops##
+#Script do banco de dados
+## Drops ##
 DROP DATABASE congresso;
 DROP TABLE IF EXISTS `endereco`;
 DROP TABLE IF EXISTS `participante`;
@@ -9,11 +9,11 @@ DROP TABLE IF EXISTS `artigo_autor`;
 DROP TABLE IF EXISTS `revisao`;
 DROP TABLE IF EXISTS `artigo_revisao`;
 
---##Creates##
+## Creates ##
 CREATE DATABASE congresso;
 
 CREATE TABLE `endereco` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint NOT NULL auto_increment,
   `rua` varchar(255) default NULL,
   `bairro` varchar(10) default NULL,
   `cidade` varchar(255),
@@ -23,25 +23,28 @@ CREATE TABLE `endereco` (
 ) AUTO_INCREMENT=1;
 
 CREATE TABLE `participante` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint NOT NULL auto_increment,
   `nome` varchar(255) default NULL,
   `telefone` varchar(100) default NULL,
   `email` varchar(255) default NULL,
   `localEmprego` varchar(255),
   `revisor` varchar(255) default NULL,
   `enderecoId` mediumint,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT FK_Endereco_Participante FOREIGN KEY (`enderecoId`) REFERENCES endereco(`id`)
 ) AUTO_INCREMENT=1;
 
 CREATE TABLE `cartao` (
   `numero` varchar(255),
   `dataVencimento` varchar(255),
   `marca` varchar(255),
-  `participanteId` mediumint
+  `participanteId` mediumint,
+  PRIMARY KEY(`numero`, dataVencimento),
+  CONSTRAINT FK_Participante_Cartao FOREIGN KEY (`participanteId`) REFERENCES participante(`id`)
 );
 
 CREATE TABLE `artigo` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint NOT NULL auto_increment,
   `titulo` TEXT default NULL,
   `resumo` TEXT default NULL,
   `pdf` mediumint default NULL,
@@ -51,30 +54,38 @@ CREATE TABLE `artigo` (
 CREATE TABLE `artigo_autor` (
   `artigoId` mediumint,
   `participanteId` mediumint,
+   PRIMARY KEY(`artigoId`, `participanteId`),
+   CONSTRAINT FK_Participante_ArtigoAutor FOREIGN KEY (`participanteId`) REFERENCES participante(`id`),
+   CONSTRAINT FK_Artigo_ArtigoAutor FOREIGN KEY (`artigoId`) REFERENCES artigo(`id`)
 ) AUTO_INCREMENT=1;
 
 CREATE TABLE `revisao` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint NOT NULL auto_increment,
   `artigoId` mediumint,
   `participanteId` mediumint,
   `nota` mediumint default NULL,
   `comentario` TEXT default NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  CONSTRAINT FK_Participante_Revisao FOREIGN KEY (`participanteId`) REFERENCES participante(`id`),
+  CONSTRAINT FK_Artigo_Revisao FOREIGN KEY (`artigoId`) REFERENCES artigo(`id`)
 ) AUTO_INCREMENT=1;
 
 CREATE TABLE `artigo_revisao` (
   `artigoId` mediumint,
-  `revisaoId` mediumint
+  `revisaoId` mediumint,
+   PRIMARY KEY (`artigoId`, `revisaoId`),
+   CONSTRAINT FK_Artigo_ArtigoRevisao FOREIGN KEY (`artigoId`) REFERENCES artigo(`id`),
+   CONSTRAINT FK_Revisao_ArtigoRevisao FOREIGN KEY (`revisaoId`) REFERENCES revisao(`id`)
 );
 
-
---##Inserts##
 /*
+#Inserts#
 INSERT INTO endereco (rua, bairro, cidade, estado, complemento) VALUES ('Prof Jaimede Sá Menezes','Cajazeiras','Salvador','BA','Prox ao LFMC');
 INSERT INTO participante(nome, telefone, email, localEmprego, revisor, enderecoId) VALUES ('Jamille','71991121926','mille.bittencourt@gmail.com','Solutis Tec',1,1);
-INSERT INTO cartao(numeroCartao, dataVencimento, marcaCartao, participanteId) VALUES ('4400890087009900','20200413','MasterCard',2);
+INSERT INTO cartao(numero, dataVencimento, marca, participanteId) VALUES ('4400890087009900','20200413','MasterCard',1);
 INSERT INTO artigo(titulo, resumo, pdf) VALUES ('Utilização de QR Code na Acessibilidaade','Um resumo bem curto',9);
-INSERT INTO artigo_autor(artigoId, participanteId) VALUES (1,2);
-INSERT INTO revisao(artigoId, participanteId, nota, comentario) VALUES (1,2,8.5,'Bom artigo');
+INSERT INTO artigo_autor(artigoId, participanteId) VALUES (1,1);
+INSERT INTO revisao(artigoId, participanteId, nota, comentario) VALUES (1,1,8.5,'Bom artigo');
 INSERT INTO artigo_revisao (artigoId, revisaoId) VALUES (1,1);
 */
+
